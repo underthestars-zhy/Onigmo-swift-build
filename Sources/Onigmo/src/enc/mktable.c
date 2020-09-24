d0dd20ecbad3aa0de7083c3e522ef722ed738b8a
Makoto Aoyama <m@aoyama.dev>
2020-09-24 23:29:19 +0900

    Fix duplicate symbol

--- Sources/Onigmo/src/enc/mktable.c
+++ Sources/Onigmo/src/enc/mktable.c
@@ -1160,25 +1160,25 @@ static int exec(FILE* fp, ENC_INFO* einfo)
   return 0;
 }
 
-extern int main(int argc ARG_UNUSED, char* argv[] ARG_UNUSED)
-{
-  int r;
-  int i;
-  FILE* fp = stdout;
-
-  setlocale(LC_ALL, "C");
-  /* setlocale(LC_ALL, "POSIX"); */
-  /* setlocale(LC_ALL, "en_GB.iso88591"); */
-  /* setlocale(LC_ALL, "de_BE.iso88591"); */
-  /* setlocale(LC_ALL, "fr_FR.iso88591"); */
-
-  for (i = 0; i < (int )(sizeof(Info)/sizeof(ENC_INFO)); i++) {
-    r = exec(fp, &Info[i]);
-    if (r < 0) {
-      fprintf(stderr, "FAIL exec(): %d\n", r);
-      return -1;
-    }
-  }
-
-  return 0;
-}
+//extern int main(int argc ARG_UNUSED, char* argv[] ARG_UNUSED)
+//{
+//  int r;
+//  int i;
+//  FILE* fp = stdout;
+//
+//  setlocale(LC_ALL, "C");
+//  /* setlocale(LC_ALL, "POSIX"); */
+//  /* setlocale(LC_ALL, "en_GB.iso88591"); */
+//  /* setlocale(LC_ALL, "de_BE.iso88591"); */
+//  /* setlocale(LC_ALL, "fr_FR.iso88591"); */
+//
+//  for (i = 0; i < (int )(sizeof(Info)/sizeof(ENC_INFO)); i++) {
+//    r = exec(fp, &Info[i]);
+//    if (r < 0) {
+//      fprintf(stderr, "FAIL exec(): %d\n", r);
+//      return -1;
+//    }
+//  }
+//
+//  return 0;
+//}
